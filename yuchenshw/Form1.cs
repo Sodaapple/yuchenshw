a033a7ab7a9d8b1aa688afcaa2ca6239e8e771f5
Sodaapple <sodaapple7@gmail.com>
2023-04-10 03:07:34 +0800

    加入專案檔案。

---
+++ yuchenshw/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yuchenshw
+{
+    public partial class Form1 : Form
+    {
+        MyDBHelper MyDB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MyDB = new MyDBHelper(Properties.Settings.Default.yuchenshwConnectionString, "phonebook");
+            DataTable dt = MyDB.Select();
+            dataGridView1.DataSource = dt;
+
+        }
+        //txt各欄位值
+
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("@empid", txt_empid.Text.Trim());
+            dic.Add("@empname", txt_empname.Text.Trim());
+            dic.Add("@empphone", txt_empphone.Text.Trim());
+
+            sql = $"select * from phonebook where empid like '%{dic["@empid"]}%' and empname like '%{dic["@empname"]}%' and empphone like '%{dic["@empphone"]}%'";
+
+            DataTable dt = MyDB.Select(sql);
+            dataGridView1.DataSource = dt;
+        }
+
+
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ yuchenshw/MyDBHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuchenshw
+{
+    internal class MyDBHelper
+    {
+        //請出連線字串
+        private SqlConnection conn;
+        private SqlDataAdapter adapter;
+        private SqlCommandBuilder builder;
+        private DataTable dt;
+
+        //連線字串、Table名稱
+        public MyDBHelper(string connstr, string initDataTableName)
+        {
+            conn = new SqlConnection(connstr);
+            adapter = new SqlDataAdapter($"select * from {initDataTableName}", conn);
+            builder = new SqlCommandBuilder(adapter);
+        }
+        //填入table值至欄位(無條件)
+        public DataTable Select()
+        {
+            dt = new DataTable();
+            adapter.FillSchema(dt, SchemaType.Mapped);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        //update該table可能會用到的參數
+        public string[] GetUpdateCommandParameters()
+        {
+
+            builder.ConflictOption = ConflictOption.OverwriteChanges;
+            SqlParameterCollection temp = builder.GetUpdateCommand(true).Parameters;
+            string[] paramList = new string[temp.Count];
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                paramList[i] = temp[i].ParameterName;
+            }
+            return paramList;
+        }
+
+        //填入table值至欄位(依條件)
+        public DataTable Select(string sql)
+        {
+            adapter.SelectCommand = new SqlCommand(sql, conn);
+            dt = new DataTable();
+            adapter.FillSchema(dt, SchemaType.Mapped);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+
+    }
+}
