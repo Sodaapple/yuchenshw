224ecde995ec29185a22a8e8cd4391e748822dae
Sodaapple <sodaapple7@gmail.com>
2023-04-10 03:22:53 +0800

    調整

--- yuchenshw/FormHW.Designer.cs
+++ yuchenshw/FormHW.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace yuchenshw
 {
-    partial class Form1
+    partial class FormHW
     {
         /// <summary>
-        /// 設計工具所需的變數。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清除任何使用中的資源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,15 +20,14 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            //this.tableAdapterManager1 = new yuchenshw.yuchenshwDataSetTableAdapters.TableAdapterManager();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btn_delete = new System.Windows.Forms.Button();
@@ -42,136 +41,191 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableAdapterManager1
-            // 
-            //this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
-            //this.tableAdapterManager1.Connection = null;
-            //this.tableAdapterManager1.phonebookTableAdapter = null;
-            //this.tableAdapterManager1.UpdateOrder = yuchenshw.yuchenshwDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 78);
+            this.label4.Location = new System.Drawing.Point(44, 79);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 15);
-            this.label4.TabIndex = 21;
+            this.label4.TabIndex = 31;
             this.label4.Text = "篩選條件：";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.btn_delete);
             this.groupBox1.Controls.Add(this.btn_add);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(638, 124);
+            this.groupBox1.Location = new System.Drawing.Point(598, 134);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(244, 370);
-            this.groupBox1.TabIndex = 20;
+            this.groupBox1.Size = new System.Drawing.Size(258, 326);
+            this.groupBox1.TabIndex = 30;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "新增、修改、刪除";
             // 
             // btn_delete
             // 
-            this.btn_delete.Location = new System.Drawing.Point(15, 315);
+            this.btn_delete.Location = new System.Drawing.Point(22, 267);
             this.btn_delete.Name = "btn_delete";
-            this.btn_delete.Size = new System.Drawing.Size(90, 40);
+            this.btn_delete.Size = new System.Drawing.Size(100, 40);
             this.btn_delete.TabIndex = 11;
             this.btn_delete.Text = "刪除";
             this.btn_delete.UseVisualStyleBackColor = true;
             // 
             // btn_add
             // 
-            this.btn_add.Location = new System.Drawing.Point(15, 269);
+            this.btn_add.Location = new System.Drawing.Point(22, 210);
             this.btn_add.Name = "btn_add";
-            this.btn_add.Size = new System.Drawing.Size(90, 40);
+            this.btn_add.Size = new System.Drawing.Size(100, 40);
             this.btn_add.TabIndex = 8;
             this.btn_add.Text = "新增";
             this.btn_add.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(111, 269);
+            this.button2.Location = new System.Drawing.Point(135, 210);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 40);
+            this.button2.Size = new System.Drawing.Size(100, 40);
             this.button2.TabIndex = 9;
             this.button2.Text = "修改";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(588, 55);
+            this.btn_search.Location = new System.Drawing.Point(598, 56);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(90, 40);
-            this.btn_search.TabIndex = 19;
+            this.btn_search.Size = new System.Drawing.Size(100, 40);
+            this.btn_search.TabIndex = 29;
             this.btn_search.Text = "查詢";
             this.btn_search.UseVisualStyleBackColor = true;
             this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
             // txt_empphone
             // 
-            this.txt_empphone.Location = new System.Drawing.Point(383, 70);
+            this.txt_empphone.Location = new System.Drawing.Point(393, 71);
             this.txt_empphone.Name = "txt_empphone";
             this.txt_empphone.Size = new System.Drawing.Size(179, 25);
-            this.txt_empphone.TabIndex = 18;
+            this.txt_empphone.TabIndex = 28;
             // 
             // txt_empname
             // 
-            this.txt_empname.Location = new System.Drawing.Point(241, 70);
+            this.txt_empname.Location = new System.Drawing.Point(251, 71);
             this.txt_empname.Name = "txt_empname";
             this.txt_empname.Size = new System.Drawing.Size(130, 25);
-            this.txt_empname.TabIndex = 17;
+            this.txt_empname.TabIndex = 27;
             // 
             // txt_empid
             // 
-            this.txt_empid.Location = new System.Drawing.Point(131, 70);
+            this.txt_empid.Location = new System.Drawing.Point(141, 71);
             this.txt_empid.Name = "txt_empid";
             this.txt_empid.Size = new System.Drawing.Size(100, 25);
-            this.txt_empid.TabIndex = 16;
+            this.txt_empid.TabIndex = 26;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(380, 47);
+            this.label3.Location = new System.Drawing.Point(390, 48);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 15);
-            this.label3.TabIndex = 15;
+            this.label3.TabIndex = 25;
             this.label3.Text = "員工電話";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(238, 47);
+            this.label2.Location = new System.Drawing.Point(248, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 15);
-            this.label2.TabIndex = 14;
+            this.label2.TabIndex = 24;
             this.label2.Text = "員工名稱";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 47);
+            this.label1.Location = new System.Drawing.Point(138, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 15);
-            this.label1.TabIndex = 13;
+            this.label1.TabIndex = 23;
             this.label1.Text = "員工代號";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(46, 124);
+            this.dataGridView1.Location = new System.Drawing.Point(47, 134);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(566, 370);
-            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.Size = new System.Drawing.Size(525, 335);
+            this.dataGridView1.TabIndex = 22;
             // 
-            // Form1
+            // textBox1
             // 
-            this.ClientSize = new System.Drawing.Size(928, 540);
+            this.textBox1.Location = new System.Drawing.Point(96, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 25);
+            this.textBox1.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "員工代號";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "員工名稱";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 25);
+            this.textBox2.TabIndex = 34;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 25);
+            this.textBox3.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 15);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "員工電話";
+            // 
+            // FormHW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 520);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_search);
@@ -182,9 +236,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
-            this.Text = "員工電話簿";
+            this.Name = "FormHW";
+            this.Text = "FormHW";
+            this.Load += new System.EventHandler(this.FormHW_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -192,11 +248,7 @@
         }
 
         #endregion
-        //private yuchenshwDataSet yuchenshwDataSet;
-        //private System.Windows.Forms.BindingSource phonebookBindingSource;
-        //private yuchenshwDataSetTableAdapters.phonebookTableAdapter phonebookTableAdapter;
-        //private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
-        //private yuchenshwDataSetTableAdapters.TableAdapterManager tableAdapterManager1;
+
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btn_delete;
@@ -210,6 +262,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
-
---
+++ yuchenshw/FormHW.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yuchenshw
+{
+    public partial class FormHW : Form
+    {
+        MyDBHelper MyDB;
+
+        public FormHW()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHW_Load(object sender, EventArgs e)
+        {
+            MyDB = new MyDBHelper(Properties.Settings.Default.yuchenshwConnectionString, "phonebook");
+            DataTable dt = MyDB.Select();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("@empid", txt_empid.Text.Trim());
+            dic.Add("@empname", txt_empname.Text.Trim());
+            dic.Add("@empphone", txt_empphone.Text.Trim());
+
+            sql = $"select * from phonebook where empid like '%{dic["@empid"]}%' and empname like '%{dic["@empname"]}%' and empphone like '%{dic["@empphone"]}%'";
+
+            DataTable dt = MyDB.Select(sql);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
